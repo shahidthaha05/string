a809d75f8b7b8e1e0073fa14f2b43caf558add22
abinkrishna <abin48515@gmail.com>
2024-12-18 16:01:26 +0530

    string

---
+++ str.py
@@ -0,0 +1,37 @@
+# a='helo'
+# print(a[-1])
+# print(a[2:])
+# print(a[:1])
+
+
+# slice
+
+# a='shahidthaha'
+# print(a[1:4])
+# print(a[4:11])
+# print(a[::2])
+# print(a[::-1])
+
+# format
+# print("{:<10}{:<10}".format("hello","welcome"))
+
+
+# capitalize
+
+a='welcome'
+b='good morning'
+# print(a.capitalize())
+# print(a.upper()) make whole string cap letters
+# print(b.lower()) make whole string lower case
+# print(a.isupper())  true or false 
+# print(a.islower()) 
+# print(a.isalpha()) checking letter or not
+# print(a.isdigit()) checking its digit or not
+
+
+# print(a.index('e',0))  find the letter and 0 for starting position of array
+# print(a.count('e'))  count of the letter
+# print(a.center(20))  takes 20 space from the starting
+# print(a.strip()) removing white space of the string
+# print(a.startswith('w')) finding the first letter true or not
+# print(b.replace('morning','night')) changing the values
---
+++ task.py
@@ -0,0 +1,27 @@
+# a='malayalam'
+# rev=''
+# for i in a:
+#     rev=i+rev
+    
+
+# if rev==a:
+#     print('its a pallindrome')
+# else:
+#     print('its not a pallindrome')
+    
+        
+        
+        
+a='Welcome'
+count=0
+cap=0
+lower=0
+for i in a:
+    if i.isalpha():
+        count+=1
+        
+
+print("total number":,count)
+    
+    
+    
